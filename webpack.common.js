c74975cd8fbd7bb6d921089c2bf4e3f52485fbc7
RstK <208619198+bowman025@users.noreply.github.com>
2025-08-25 15:33:13 +0200

    Fix issue with loading style to js

--- webpack.common.js
+++ webpack.common.js
@@ -11,6 +11,18 @@ module.exports = {
             title:"Production",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
